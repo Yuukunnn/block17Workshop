15a69b1deb8e529611051289afe6f72f12a123ff
Yukun Zhang <yukun@yukuns-mbp.mynetworksettings.com>
2024-04-12 14:20:07 -0400

    block17 completed

--- index.js
+++ index.js
@@ -11,27 +11,37 @@ class Numbers {
   }
   count() {
     //return the count of numbers in data
+    return this.data.length;
   }
   printNumbers() {
     //print the numbers in data
+    this.data.forEach ((number,index) => {
+      console.log(`Index ${index} : ${number}`)
+    })
   }
   odds() {
     //return the odd numbers in data
+     return this.data.filter (number => number % 2 !== 0);
   }
   evens() {
     //return the even numbers in data
+    return this.data.filter (number => number % 2 ===0)
   }
   sum() {
     //return the sum of the numbers
+    return this.data.reduce ((sumTotal, number) => sumTotal + number, 0)
   }
   product() {
     //return the product of the numbers
+    return this.data.reduce ((product, number) => product * number, 1)
   }
   greaterThan(target) {
     //return the numbers greater than the target
+    return this.data.filter((number) => number > target);
   }
   howMany(target) {
     //return the count of a given number
+    return this.data.filter((number => number === target)).length;
   }
 }
 
@@ -40,6 +50,7 @@ const str = prompt("enter some numbers, like this", "1,2,3,3,5,9");
 
 //create an instance of numbers
 const n1 = new Numbers(str);
+console.log(n1);
 console.log(n1.count()); //returns count of numbers
 n1.printNumbers(); //prints the number along with their indexes
 console.log(n1.odds()); //returns odd numbers
